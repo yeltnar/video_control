107b08d208587623d90b92de68a91a5795d4a96f
Andrew Brantley <github@andbrant.com>
2020-10-08 01:21:53 -0500

    add sub url thing

---
+++ docs/build/precache-manifest.e8ddc259188e7fc660654b8b2b42db8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909df7147afa04da3a88df9e1135c7f3",
+    "url": "/video_control/index.html"
+  },
+  {
+    "revision": "22109e75a6f99248f7bf",
+    "url": "/video_control/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bd95788dcb4aa0fb8f5",
+    "url": "/video_control/static/js/2.0bc33dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video_control/static/js/2.0bc33dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22109e75a6f99248f7bf",
+    "url": "/video_control/static/js/main.e7e74c8c.chunk.js"
+  },
+  {
+    "revision": "9ffd68ba1b855fc5db5e",
+    "url": "/video_control/static/js/runtime-main.585c00e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video_control/static/media/logo.5d5d9eef.svg"
+  }
+]);
