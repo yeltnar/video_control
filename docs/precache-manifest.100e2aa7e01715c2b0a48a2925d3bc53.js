0559f91723844d019a8ac014cd442aee4e568a9c
Andrew Brantley <github@andbrant.com>
2020-10-08 01:15:49 -0500

    first

---
+++ docs/precache-manifest.100e2aa7e01715c2b0a48a2925d3bc53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2b097699be95d1a2fe821b2d26dc06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f5a8bfa8cb8f0efed0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bd95788dcb4aa0fb8f5",
+    "url": "/static/js/2.0bc33dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bc33dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f5a8bfa8cb8f0efed0",
+    "url": "/static/js/main.bf4ce165.chunk.js"
+  },
+  {
+    "revision": "68675e3da70cec138e63",
+    "url": "/static/js/runtime-main.c82eb1f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const url = "wss://Node-WSS.yeltnar.repl.co";
+let socket = new WebSocket(url);
+
+socket.og={};
+socket.og_send = socket.send;
+socket.send = (...a)=>{
+  socket.og_send(...a);
+  console.log(...a);
+};
+
+socket.onopen = function (e) {
+  console.log("asdf")
+};
+
 function App() {
+
+  const [video_url,setVideoUrl] = useState("");
+
+  console.log({video_url})
+
+  function play(){
+    console.log({video_url})
+    socket.send(JSON.stringify(getPlayAction(video_url)));
+  }
+  function pause(){
+    console.log({video_url})
+    socket.send(JSON.stringify(getPauseAction(video_url)));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={video_url} onChange={x=>{setVideoUrl(x.target.value)}}></input>
+      <button onClick={pause}>pause</button>
+      <button onClick={play}>play</button>
     </div>
   );
 }
 
 export default App;
+
+function getPlayAction(url) {
+
+  return {
+      "videocontrol": true,
+      "action": "play",
+      url,
+  }
+}
+
+function getPauseAction(url) {
+
+  return {
+      "videocontrol": true,
+      "action": "pause",
+      url,
+  }
+}
